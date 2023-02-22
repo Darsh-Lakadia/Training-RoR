e513f947b3df2107f987c7d1db13d02321dcc2f2
DarshLakadia <76402096+DarshLakadia@users.noreply.github.com>
2023-02-22 16:00:57 +0530

    Add files via upload

---
+++ 21-feb/assignment-6-2.rb
@@ -0,0 +1,74 @@
+require 'json'
+file = File.open("phonebook.txt","r")
+dictonary = eval(file.read)
+puts dictonary
+puts "Enter your Choice"
+puts "1. Find number with person's name"
+puts "2. Save New number with name"
+puts "3. Delete number"
+choice = gets.chomp.to_i
+write_to_file = lambda{
+  write_file=File.open("phonebook.txt","w") do |file|
+    file.puts dictonary.to_s
+  end  
+}
+
+case 
+when choice ==1
+  puts "Enter name:"
+  name = gets.chomp
+  if dictonary.has_key?name
+    puts dictonary[name]
+  else
+    puts "No name found"
+  end
+when choice ==2
+  puts "2. choice"
+  puts "Enter Name"
+  name = gets.chomp
+  puts "Enter mobile number"
+  number = gets.chomp
+
+  if dictonary.has_key?name
+    puts "Already name is present"
+
+  elsif dictonary.key(number) == nil
+    hash = {}
+    hash[name] = number  
+    dictonary=dictonary.merge(hash)
+    write_to_file.call
+  else
+    puts "Already number is present"
+  end
+when choice ==3
+  puts "Enter Choice"
+  puts "1. Delete by Name"
+  puts "2. Delete by Number"
+  option = gets.chomp.to_i
+  case 
+  when option ==1
+    puts "Enter Name you want to delete"
+    name = gets.chomp
+    if dictonary.has_key?name
+      dictonary.delete(name)
+      write_to_file.call      
+    else
+      puts "Not Found Name"
+    end
+  when option ==2
+    puts "Enter Number you want to delete"
+    number = gets.chomp
+    if dictonary.has_key?dictonary.key(number)
+      dictonary.delete(dictonary.key(number))
+      write_to_file.call      
+    else
+      puts "Not Number Found"
+    end
+  end
+else
+  puts "nothing..."
+end
+# find_number = Proc.new{ 
+#   |name|
+#   while 
+# }
---
+++ 21-feb/assignment-6-3.rb
@@ -0,0 +1,7 @@
+elements = ['two', 'three', 'one']
+hash_to_be_sorted = {'three' => 3, 'two' => 2, 'one' => 1}
+result = {}
+proc = Proc.new{
+  puts elements.map{|k| [k,hash_to_be_sorted[k]] }.to_h
+}
+proc.call
