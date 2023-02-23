758a273b6ea9fed21becd44105f4d277a16ff7ae
Darsh-Lakadia <76402096+Darsh-Lakadia@users.noreply.github.com>
2023-02-23 17:35:09 +0530

    Update assignment1-1.rb

--- 13-feb/assignment1-1.rb
+++ 13-feb/assignment1-1.rb
@@ -1,5 +1,5 @@
 SHOP_NAME = "Lakadia Computers"
-COMPANY_NAME = "Lakadia Brothers"
+$company_name = "Lakadia & Sons"
 class Product
 	def initialize(name,price)
 		@product_name = name
@@ -14,14 +14,12 @@ class Product
 	def gstcalc()
 		puts "Welcome to #{SHOP_NAME}"
 		puts "Welcome #{@name}. Your invoice with #{@gst}% gst are below"
-		@calc = (@gst * @product_price / 100)
-		@gst_price = @product_price + @calc
+		calc = (@gst * @product_price )/ 100
+		@gst_price = @product_price + calc
 		puts "product details"
-		print "#{COMPANY_NAME} #{@product_name} - #{@gst_price} \n" 
+		print "#{$company_name} #{@product_name} - #{@gst_price} \n" 
 	end
 end
 
 product = Product.new('cpu',100)
 product.gstcalc()
-#product1.get_user
-#product.get_user
