ff2bba6e2c437e716515e018ce1547ff6200be9b
Darsh-Lakadia <76402096+Darsh-Lakadia@users.noreply.github.com>
2023-02-24 21:01:56 +0530

    updat layer_time variable variable to constant 2

--- 15-feb/assignment3-1 lasagna.rb
+++ 15-feb/assignment3-1 lasagna.rb
@@ -1,18 +1,16 @@
 class Lasagna
-	EXPECTED_MINUTES_IN_OVEN = 40
-	def remaining_minutes_in_oven(actual_minutes_in_oven)
-		actual_min = actual_minutes_in_oven
-		EXPECTED_MINUTES_IN_OVEN - actual_min 	
-	end
-	def preparation_time_in_minutes(number_of_layers)
-		one_layer_time = 2
-		layer_min = number_of_layers
-		layer_time = layer_min*one_layer_time
-	end 
-	def total_time_in_minutes(number_of_layers,actual_minutes_in_oven)
-		result = remaining_minutes_in_oven(actual_minutes_in_oven) + preparation_time_in_minutes(number_of_layers)
-		p result
-	end
+  EXPECTED_MINUTES_IN_OVEN = 40
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    actual_min = actual_minutes_in_oven
+    EXPECTED_MINUTES_IN_OVEN - actual_min 	
+  end
+  def preparation_time_in_minutes(number_of_layers)
+    layer_time = number_of_layers*2
+  end 
+  def total_time_in_minutes(number_of_layers,actual_minutes_in_oven)
+    result = remaining_minutes_in_oven(actual_minutes_in_oven) + preparation_time_in_minutes(number_of_layers)
+    p result
+  end
 end
 
 lasagna = Lasagna.new
